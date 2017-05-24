5a363c195df878c79f21971af13d324a3e0a26a2
Qingling Zhang <qinglingzhang@188-151.priv23.nus.edu.sg>
2017-05-24 21:06:53 +0800

    add zoom in animation

--- draw.py
+++ draw.py
@@ -15,10 +15,6 @@ def static_draw(ax, RGB, n_max=10):
 	colors = [RGB*x for x in np.tanh(np.linspace(0,n_max/15,n_max+1))]
 
 	n = np.array(Mand.iterate(n_max, h, x0, y0), dtype=int)
-	print(n)
-	if np.max(n) > n_max:
-		print(np.max(n))
-	print("safe", n_max)
 	color_mat = np.zeros([N,M,3])
 	for i in range(N):
 		for j in range(M):
@@ -36,10 +32,10 @@ def iter_animate(fig, ax, RGB, iter_times = 10):
 	img = ax.imshow(color_mat, cmap="gray", interpolation="nearest", extent=extent)
 	text = ax.text(1.1, 2.3, "")
 	h = 0.01
-	x0 = -1
-	y0 = -1
+	x0 = 0.
+	y0 = 0.
 
-	colors = [RGB*x for x in np.tanh(np.linspace(0,iter_times/5,iter_times+1))]
+	colors = [RGB*x for x in np.linspace(0,iter_times/5,iter_times+1)]
 
 	def init():
 		pass
@@ -54,28 +50,28 @@ def iter_animate(fig, ax, RGB, iter_times = 10):
 		
 	def data_gen():
 		for i in range(iter_times):
+			print("iteration times = {}".format(i+1))
 			n = np.array(Mand.iterate(i, h, x0, y0), dtype=int)[:]
-			if np.max(n) > i+1:
-				print(i)
-				print(np.max(n))
-				quit()
 			yield (n, i)
 
 
 	anim = animation.FuncAnimation(fig, update, data_gen, init_func=init, interval=500)
-	# anim.save('Mandelbrot_colored2.gif', writer='imagemagick', fps=300)
-	plt.show()
+	anim.save('Mandelbrot_colored2.gif', writer='imagemagick', fps=300)
+	# plt.show()
 
 def zoom_in_animate(fig, ax):
-	h = 0.01
-	scale_rate = 5
-	x = b
+	extent = [-2.2, 2.2, -2.2, 2.2]
+	color_mat = np.zeros([N,M,3]) 
+	img = ax.imshow(color_mat, cmap="gray", interpolation="nearest", extent=extent)
+	text = ax.text(-0.2, 2.3, "")
+
 
 	def init():
 		pass
 	def update(data):
-		n, i = data
-		text.set_text("iteration times = {}".format(i+1))
+		n, i, n_max, x = data
+		colors = [RGB*x for x in np.linspace(0,1,n_max+1)]
+		text.set_text("iteration times = {0}; frames count = {1}; x = {2:.3f}".format(n_max, i, x))
 		for i in range(N):
 			for j in range(M):
 				color_mat[i,j] = colors[n[i,j]][:]
@@ -83,17 +79,25 @@ def zoom_in_animate(fig, ax):
 		return (text, img)
 		
 	def data_gen():
-		for i in range(iter_times):
-			n = np.array(Mand.iterate(i, h, x0, y0), dtype=int)[:]
-			if np.max(n) > i+1:
-				print(i)
-				print(np.max(n))
-				quit()
-			yield (n, i)
+		q = np.array([0.,0.])
+		h = 0.005
+		n_max = min(int(0.8/h), 120)
+		scale_rate = 1.2
+		v = np.array([-0.01, 0])
+		for i in range(1000):
+			print("current t = {}".format(i+1))
+			if q[0] > -1.42:
+				n = np.array(Mand.iterate(n_max, h, q[0], q[1]), dtype=int)[:]
+				q = q+ i*v
+			else:
+				h = h/scale_rate
+				n_max = min(int(0.8/h), 400)
+				n = np.array(Mand.iterate(n_max, h, q[0], q[1]), dtype=int)[:]
+			yield (n, i, n_max, q[0])
 
 
 	anim = animation.FuncAnimation(fig, update, data_gen, init_func=init, interval=500)
-	# anim.save('Mandelbrot_colored2.gif', writer='imagemagick', fps=300)
+	# anim.save('Mandelbrot_zoom.gif', writer='imagemagick', fps=300)
 	plt.show()
 
 
@@ -103,5 +107,6 @@ def zoom_in_animate(fig, ax):
 if __name__=="__main__":
 	fig, ax = plt.subplots(ncols=1, figsize=[9,9])
 	RGB = np.array([0.5,1,0.5])
-	static_draw(ax, RGB, n_max=120)
-	# iter_animate(fig, ax, RGB = RGB, iter_times=30)
+	# static_draw(ax, RGB, n_max=120)
+	# iter_animate(fig, ax, RGB = RGB, iter_times=50)
+	zoom_in_animate(fig, ax)
